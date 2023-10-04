8c4ed69d61f7c71db5e8522fb54a81c3e43150a8
Juani Espinosa <juaniespinosa@gmail.com>
2023-10-04 11:18:51 -0300

    Platform Work in progress

--- platform/main.tf
+++ platform/main.tf
@@ -56,12 +56,52 @@ module "gke_standard" {
 
 }
 
-module "kubernetes" {
-  source = "../modules/kubernetes"
+# module "kubernetes" {
+#   source = "../modules/kubernetes"
 
-  depends_on       = [module.gke_standard]
-  region           = var.region
-  cluster_name     = var.cluster_name
-  enable_autopilot = var.enable_autopilot
-  enable_tpu       = var.enable_tpu
+#   depends_on       = [module.gke_standard]
+#   region           = var.region
+#   cluster_name     = var.cluster_name
+#   enable_autopilot = var.enable_autopilot
+#   enable_tpu       = var.enable_tpu
+# }
+
+########################################################################
+########################################################################
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "kubernetes" {
+  host  = data.google_container_cluster.ml_cluster.endpoint
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
+  )
+}
+
+provider "kubectl" {
+  host  = data.google_container_cluster.ml_cluster.endpoint
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
+  )
+}
+
+provider "helm" {
+  kubernetes {
+    ##config_path = pathexpand("~/.kube/config")
+    host  = data.google_container_cluster.ml_cluster.endpoint
+    token = data.google_client_config.provider.access_token
+    cluster_ca_certificate = base64decode(
+      data.google_container_cluster.ml_cluster.master_auth[0].cluster_ca_certificate
+    )
+  }
 }
